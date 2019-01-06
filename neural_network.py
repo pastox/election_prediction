13c3d0b68cfcf37005ce9317258a39fd5c74128f
GABRION Nicolas <nico.gabrion@gmail.com>
2019-01-06 09:52:42 +0100

    '-'

--- neural_network.py
+++ neural_network.py
@@ -18,6 +18,8 @@ from keras.layers.core import  Lambda , Dense, Flatten, Dropout
 from keras.callbacks import EarlyStopping
 from keras.layers import BatchNormalization, Convolution2D , MaxPooling2D
 
+from copy import deepcopy
+
 ##cross validation
 def c_validation(X, y, k, function):
     kf = cross_validation.KFold(X.shape[0], n_folds=k)
@@ -25,6 +27,11 @@ def c_validation(X, y, k, function):
     totalloss = 0 # Variable that will store the total intances that will be tested  
     totalsuccess5 = 0
     totalsuccess10 = 0	
+    totalpercentageloss = 0
+    res=[]
+    corr=[]
+    
+    lines=[]
     
     for trainIndex, testIndex in kf:
         trainSet = X[trainIndex]
@@ -32,58 +39,98 @@ def c_validation(X, y, k, function):
         trainLabels = y[trainIndex]
         testLabels = y[testIndex]
         
+        avg = 0
+        for i in trainLabels:
+            avg+=i
+        avg=avg/trainLabels.shape[0]
+        
         predictedLabels = function(trainSet, trainLabels, testSet)
     
         loss = 0
+        percentageloss=0
         success5 = 0
         success10 = 0	
         for i in range(testSet.shape[0]):
             if not np.isnan(predictedLabels[i][0]):
-                loss+=abs(predictedLabels[i][0]-testLabels[i])/(testLabels.shape[0])
+                if predictedLabels[i][0]>1:
+                    predictedLabels[i][0]=avg
+                loss+=abs(predictedLabels[i][0]-testLabels[i])/(testLabels[i]*testLabels.shape[0])
+                percentageloss+=abs(predictedLabels[i][0]-testLabels[i])/testLabels.shape[0]
                 if abs(predictedLabels[i][0]-testLabels[i])/testLabels[i] < 0.05:
                     success5+=1/testLabels.shape[0]
+                    success10+=1/testLabels.shape[0]
                 elif abs(predictedLabels[i][0]-testLabels[i])/testLabels[i] < 0.1:
                     success10+=1/testLabels.shape[0]
-        print ('Loss: ', loss)
-        print ('Success 0.05: ',success5)
-        print ('Success 0.1: ',success10)
+            else:
+                print(i)
+        print ('Loss: ', 100*loss, '%')
+        print ('Average error: ', 100*percentageloss, '%')
+        print ('Success 0.05: ',100*success5, '%')
+        print ('Success 0.1: ',100*success10, '%')
         totalloss += loss
+        totalpercentageloss += percentageloss
         totalsuccess5+=success5
         totalsuccess10+=success10
-    print ('Total Loss: ',totalloss/k)
-    print ('Total success 0.05: ',totalsuccess5/k)
-    print ('Total success 0.1: ',totalsuccess10/k)
+        
+        res+=list(predictedLabels)
+        corr+=list(testLabels[:])
+        
+    plt.plot(res, linestyle='', marker='.')
+    plt.plot(corr, linestyle='', marker='.')    
+    plt.show()
+        
+    print ('Total Loss: ',100*totalloss/k, '%')
+    print ('Total Average Error: ',100*totalpercentageloss/k, '%')
+    print ('Total success 0.05: ',100*totalsuccess5/k, '%')
+    print ('Total success 0.1: ',100*totalsuccess10/k, '%')
     return totalloss/k
+    #∟return lines
 
 ##data import
 
 #data = pd.read_csv('C:\\Users\\Nicolas\\Desktop\\election_prediction\\macronData.csv')
-data = np.genfromtxt('C:\\Users\\Nicolas\\Desktop\\election_prediction\\macronData.csv', delimiter=',', skip_header = 1)
+data = np.genfromtxt('C:\\Users\\Nicolas\\Desktop\\election_prediction\\lepenData.csv', delimiter=',', skip_header = 1)
+data=np.delete(data, 41, axis=1)
+nan=0
+for i in range(data.shape[0]):
+    for j in range(data.shape[1]):
+        if np.isnan(data[i,j]):
+            print(i,j)
+            nan+=1
+print(nan)
 
 X = data[:,:-1]
 y=data[:,-1]
 
 
 ##neural network
-def get_fc_model():
-    model = Sequential()
-    #model.add(Dense(64, activation='relu', input_dim=43))
-    model.add(Dense(32, activation='sigmoid', input_dim=43))
-    model.add(Dense(16, activation='sigmoid', input_dim=43))
-    model.add(Dense(8, activation='sigmoid', input_dim=43))
-    model.add(Dense(4, activation='sigmoid', input_dim=43))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-    return model
-
 
 def neural_network(Xtrain,ytrain, Xtest):
-    fc = get_fc_model()
-    fc.fit(Xtrain, ytrain, epochs=3, validation_split=0.2, verbose = 0)
-    return fc.predict(Xtest)
+    model = Sequential()
+    model.add(Dense(30, activation='relu', input_dim=42))
+    model.add(Dense(15, activation='relu', input_dim=42))
+    model.add(Dense(1, activation='linear'))
+    model.compile(optimizer='SGD',
+              loss='mean_absolute_error',
+              metrics=['accuracy'])
+    model.fit(Xtrain, ytrain, epochs=5, validation_split=0.2, verbose = 0)
+    return model.predict(Xtest)
 
 ##
-
 c_validation(X,y,5, neural_network)
+
+
+"""for i in range(10):
+    deb=1
+    l=range(2100)
+    for i in range(10):
+        print(l)
+        lines = c_validation(X,y,5, neural_network)
+        aux=[]
+        for e in lines:
+            if e in l:
+                aux.append(e)
+        l=deepcopy(aux)
+    print(l)"""
+                
+                
